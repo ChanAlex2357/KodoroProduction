83179f767bd061900106771d43c10a5d05bf1348
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-11-19 20:57:43 +0300

    creation de seeders de base pour genrer aleatoirement

---
+++ kodoro-ejb/src/mg/kodoro/helpers/seeds/IntervalleSeeds.java
@@ -0,0 +1,38 @@
+package mg.kodoro.helpers.seeds;
+
+
+public abstract class IntervalleSeeds<T> {
+    private double min;
+    private double max;
+    // Constructeur
+    public IntervalleSeeds(double min, double max) {
+        setMin(min);
+        setMax(max);
+    }
+
+    public double getMin() {
+        return min;
+    }
+    public int getIntMin(){
+        return (int) getMin();
+    }
+
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+
+    public double getMax() {
+        return max;
+    }
+    public int getIntMax(){
+        return (int) getMax();
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+
+    public abstract T generate();
+}
+
